270a8bd83ef630dae90859b8965f32be7caaf269
25f2000889 <25f2000889@ds.study.iitm.ac.in>
2026-07-27 18:52:23 +0530

    Inject enums in all routes via context processor

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, session
-
+from enums import UserRole, UserStatus, TrekDifficulty, TrekStatus, TrekBookingStatus
 from config import Config
 from db import db
 
@@ -17,11 +17,20 @@ def create_app():
     db.init_app(app)
 
     @app.context_processor
-    def inject_current_user():
+    def inject_current_user_and_enums():
         user_id = session.get("user_id")
 
         user = db.session.get(User, user_id) if user_id else None
-        return dict(current_user=user)
+        return {
+            "current_user": user,
+            "enums": {
+                "UserRole": UserRole,
+                "UserStatus": UserStatus,
+                "TrekDifficulty": TrekDifficulty,
+                "TrekStatus": TrekStatus,
+                "TrekBookingStatus": TrekBookingStatus,
+            }
+        }
 
     app.register_blueprint(auth_bp)
     app.register_blueprint(admin_bp)
