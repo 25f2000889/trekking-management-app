b6100950fc3ec61d6b9cdf0a4e44816a6ccb35f2
25f2000889 <25f2000889@ds.study.iitm.ac.in>
2026-08-01 16:04:36 +0530

    Implement Browse Treks feature with searching and sorting for User or Trekkers

--- routes/web/trekker.py
+++ routes/web/trekker.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, redirect, render_template, request, url_for, session, flash
 from decorators import auth_required, roles_required
+from enums import TrekDifficulty
+from validation import optional, optional_enum
+from services import TrekService
 
 trekker_bp = Blueprint("trekker", __name__, url_prefix="/trekker")
 
@@ -8,3 +11,16 @@ trekker_bp = Blueprint("trekker", __name__, url_prefix="/trekker")
 @roles_required("TREKKER")
 def dashboard():
     return render_template("trekker/dashboard.html", tab="dashboard")
+
+@trekker_bp.get("/treks")
+@auth_required
+@roles_required("TREKKER")
+def treks():
+    search_term = optional(request.args, "search")
+    difficulty = optional_enum(request.args, "difficulty", "Difficulty", enum_class=TrekDifficulty)
+    sort = optional(request.args, "sort")
+
+    treks = TrekService.search_treks(search_term=search_term, difficulty=difficulty, sort=sort)
+
+    form = request.args
+    return render_template("trekker/treks.html", tab="treks", treks=treks, _form=form)
--- services/trek_service.py
+++ services/trek_service.py
@@ -1,5 +1,5 @@
 from typing import Literal
-
+from enums import TrekDifficulty
 from enums import TrekStatus
 from models import Trek
 from sqlalchemy import or_
@@ -8,6 +8,32 @@ from utils import escape_search_input
 
 def get_all_treks():
     return db.session.query(Trek).all()
+
+def search_treks(search_term: str | None = None, difficulty: TrekDifficulty | None = None, sort: str | None = None) -> list[Trek]:
+    query = db.session.query(Trek).filter(Trek.status == TrekStatus.APPROVED)
+
+    if search_term:
+        search_term = escape_search_input(search_term.strip())
+        query = query.filter(
+            or_(
+                Trek.name.ilike(f"{search_term}%", escape="\\"),
+                Trek.location.ilike(f"{search_term}%", escape="\\"),
+            )
+        )
+
+    if difficulty:
+        query = query.filter(Trek.difficulty == difficulty)
+
+    if sort == "name_asc":
+        query = query.order_by(Trek.name)
+    elif sort == "name_desc":
+        query = query.order_by(Trek.name.desc())
+    elif sort == "date_asc":
+        query = query.order_by(Trek.start_date)
+    elif sort == "date_desc":
+        query = query.order_by(Trek.start_date.desc())
+
+    return query.all()
 
 def get_assigned_treks_for_staff(staff_id: int, only_statuses: list[TrekStatus] | None = None) -> list[Trek]:
     query = db.session.query(Trek).filter(Trek.assigned_staff_id == staff_id)
